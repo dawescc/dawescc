3f1d3c43884195a9fec7d872b9d5ada0ca794a6e
Ryan Dawes <ryan@dawes.cc>
2023-03-11 15:31:06 -0500

    improved mobile

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,5 +1,12 @@
 const card = document.querySelector('.card');
-
 card.addEventListener('click', function() {
     card.classList.toggle('active');
 });
+
+
+
+var screenWidth = window.innerWidth;
+if (screenWidth < 768) {
+    var scriptElement = document.querySelector('script[src="./assets/js/glow.js"]');
+    scriptElement.parentNode.removeChild(scriptElement);
+}
