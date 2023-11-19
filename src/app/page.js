b10e17bad1c829429af96119a4d4311e78a1b327
Ryan Dawes <dev@dawes.cc>
2023-11-18 19:25:02 -0500

    attempted fix

--- src/app/page.js
+++ src/app/page.js
@@ -9,9 +9,9 @@ export default function Home() {
 						<div className='w-full flex justify-around items-center relative'>
 							<div className='h-20 w-auto rounded-xl overflow-hidden border border-zinc-700 shadow-md absolute top-0 left-0 -translate-x-12 -translate-y-12'>
 								<Image
-									alt='Profile Pic'
-									className='object-cover w-full h-full'
-									src='https://neutgwolrftsfsvfhutp.supabase.co/storage/v1/object/public/blog/16bmtkif9g3i.png'
+									alt= { 'Profile Pic' }
+									className= { 'object-cover w-full h-full' }
+									src= { 'https://neutgwolrftsfsvfhutp.supabase.co/storage/v1/object/public/blog/16bmtkif9g3i.png' }
 								/>
 							</div>
 							<h1 className='text-3xl font-semibold ml-10'>Ryan Dawes</h1>
