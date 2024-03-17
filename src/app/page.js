9c463ef855b200901d33f9faf8f55b8c35c2d04d
Ryan Dawes <dev@dawes.cc>
2024-03-17 11:27:40 -0400

    added image stack

--- src/app/page.js
+++ src/app/page.js
@@ -8,12 +8,14 @@ export default function Home() {
 				href='https://twitter.com/dawescc'
 				className='group w-fit'>
 				<Image
-					className='absolute w-[100px] lg:w-[120px] aspect-square rounded-3xl rotate-6 group-hover:-rotate-3 transition-all m-5 top-0 right-0 opacity-90'
+					className='img-card z-[3] bg-green-300 rotate-3 group-hover:-rotate-3 group-hover:scale-[1.05]'
 					src='/images/Headshot.jpg'
 					height={500}
 					width={500}
 					alt="Ryan's Headshot"
 				/>
+				<div className='img-card z-[2] bg-gray-400 -rotate-6 group-hover:rotate-6 translate-x-0 group-hover:translate-x-1'></div>
+				<div className='img-card z-[1] bg-gray-700 rotate-[9deg] group-hover:-rotate-[9deg] translate-x-0 group-hover:-translate-x-1'></div>
 			</a>
 
 			<div className='flex flex-col gap-12 pt-20 px-8'>
