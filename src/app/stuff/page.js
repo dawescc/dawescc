0d201d99caba6e5e17285d66ec33b46b57a47d0b
Ryan Dawes <dev@dawes.cc>
2024-04-01 14:08:00 -0400

    added makefile, redesigned work type, added work work

--- src/app/stuff/page.js
+++ src/app/stuff/page.js
@@ -13,11 +13,14 @@ export default function Stuff() {
 		// name, description, url
 	];
 
+	const work = [
+		// name, description, tech array, url
+		["NCES", "Misc. sites under the U.S. Deptartment of Education umbrella.", ["ASP.NET 2/3/4", "Classic ASP"], "https://nces.ed.gov"],
+	];
+
 	return (
 		<main className='relative flex-[1_1_100%] overflow-hidden max-w-xl lg:max-w-4xl pb-32'>
 			<div className='flex flex-col gap-12 pt-20 px-8'>
-				<p className='paragraph text-2xl font-normal'>Below are some of my personal projects, crafted to fill a personal need or help me learn.</p>
-
 				<p>
 					You can find more of my work on{" "}
 					<a
@@ -40,59 +43,123 @@ export default function Stuff() {
 					.
 				</p>
 
-				<div className='flex flex-col gap-6'>
-					<p className='label'>Projects</p>
+				<p className='paragraph text-2xl font-normal'>Some of my personal projects, crafted to fill a personal need or help me learn.</p>
 
-					<div className='text-2xl font-normal flex flex-col'>
-						{projects.map((project, projectIndex) => (
-							<a
-								key={projectIndex}
-								href={project[3]}
-								id={project[0]}
-								className='*:py-6 clean grid grid-cols-2 hover-bg transition-all rounded-md px-2 -mx-2 text-base lg:text-2xl group'>
-								<div
-									id='project_name'
-									className='flex flex-col gap-4 lg:flex-row'>
-									<span className='inline-flex items-center'>
-										<ArrowUpRight className='transition-all -translate-y-1 group-hover:-translate-y-[6px]  group-hover:translate-x-[1px]' />{" "}
-										{project[0]}
-									</span>
+				{projects.length !== 0 && (
+					<div className='flex flex-col gap-6'>
+						<p className='label'>Projects</p>
+
+						<div className='text-2xl font-normal flex flex-col'>
+							{projects.map((project, projectIndex) => (
+								<a
+									key={projectIndex}
+									href={project[3]}
+									id={project[0]}
+									className='*:py-6 clean grid grid-cols-2 hover-bg transition-all rounded-md px-2 -mx-2 text-base lg:text-2xl group'>
+									<div
+										id='project_name'
+										className='flex flex-col gap-4 lg:flex-row'>
+										<span className='inline-flex items-center'>
+											<ArrowUpRight className='transition-all -translate-y-1 group-hover:-translate-y-[6px]  group-hover:translate-x-[1px]' />{" "}
+											{project[0]}
+										</span>
+										<span
+											id='project_tech'
+											className='flex flex-col gap-2 lg:mx-auto'>
+											{project[2].map((tech, techIndex) => (
+												<span
+													key={techIndex}
+													className='project-tech'>
+													{tech}
+												</span>
+											))}
+										</span>
+									</div>
 									<span
-										id='project_tech'
-										className='flex flex-col gap-2 lg:mx-auto'>
-										{project[2].map((tech, techIndex) => (
-											<span
-												key={techIndex}
-												className='project-tech'>
-												{tech}
-											</span>
-										))}
+										id='project_desc'
+										className='paragraph'>
+										{project[1]}
 									</span>
-								</div>
-								<span
-									id='project_desc'
-									className='paragraph'>
-									{project[1]}
-								</span>
-							</a>
-						))}
+								</a>
+							))}
+						</div>
 					</div>
-				</div>
+				)}
 
 				{misc.length !== 0 && (
 					<div className='flex flex-col gap-6'>
 						<p className='label'>Misc</p>
 						<div className='text-2xl font-normal flex flex-col'>
-							{misc.map((index) => (
+							{misc.map((project, projectIndex) => (
 								<a
-									key={index}
-									href={index[2]}
-									className='*:py-6 clean grid grid-cols-2 hover-bg transition-all rounded-md px-2 -mx-2'>
-									<span>
-										{index[0]} <ArrowUpRight />
+									key={projectIndex}
+									href={project[3]}
+									id={project[0]}
+									className='*:py-6 clean grid grid-cols-2 hover-bg transition-all rounded-md px-2 -mx-2 text-base lg:text-2xl group'>
+									<div
+										id='project_name'
+										className='flex flex-col gap-4 lg:flex-row'>
+										<span className='inline-flex items-center'>
+											<ArrowUpRight className='transition-all -translate-y-1 group-hover:-translate-y-[6px]  group-hover:translate-x-[1px]' />{" "}
+											{project[0]}
+										</span>
+										<span
+											id='project_tech'
+											className='flex flex-col gap-2 lg:mx-auto'>
+											{project[2].map((tech, techIndex) => (
+												<span
+													key={techIndex}
+													className='project-tech'>
+													{tech}
+												</span>
+											))}
+										</span>
+									</div>
+									<span
+										id='project_desc'
+										className='paragraph'>
+										{project[1]}
 									</span>
-									<span>
-										<span>{index[1]}</span>
+								</a>
+							))}
+						</div>
+					</div>
+				)}
+
+				<p className='paragraph text-2xl font-normal'>Some projects I have been a part of during my career.</p>
+				{work.length !== 0 && (
+					<div className='flex flex-col gap-6'>
+						<p className='label'>Work</p>
+						<div className='text-2xl font-normal flex flex-col'>
+							{work.map((project, projectIndex) => (
+								<a
+									key={projectIndex}
+									href={project[3]}
+									id={project[0]}
+									className='*:py-6 clean grid grid-cols-2 hover-bg transition-all rounded-md px-2 -mx-2 text-base lg:text-2xl group'>
+									<div
+										id='project_name'
+										className='flex flex-col gap-4 lg:flex-row'>
+										<span className='inline-flex items-center'>
+											<ArrowUpRight className='transition-all -translate-y-1 group-hover:-translate-y-[6px]  group-hover:translate-x-[1px]' />{" "}
+											{project[0]}
+										</span>
+										<span
+											id='project_tech'
+											className='flex flex-col gap-2 lg:mx-auto'>
+											{project[2].map((tech, techIndex) => (
+												<span
+													key={techIndex}
+													className='project-tech'>
+													{tech}
+												</span>
+											))}
+										</span>
+									</div>
+									<span
+										id='project_desc'
+										className='paragraph'>
+										{project[1]}
 									</span>
 								</a>
 							))}
