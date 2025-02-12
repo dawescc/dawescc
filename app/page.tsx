9269cb7520d97b9c71bacb39ab7b3e2397f198fb
Ryan Dawes <dev@dawes.cc>
2025-02-11 23:39:19 -0500

    added athlete & resume

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
 	return (
 		<Page title='Ryan Dawes'>
-			<PageDesc>Sr. Software Engineer</PageDesc>
+			<PageDesc>Engineer</PageDesc>
 			<PageBlock fullWidth>
 				<p className='text-pretty'>
 					<Link href='mailto:hello@dawes.cc?subject=Website%20Contact%20Request'>Contact me</Link>.
