ca50519c537a7d888a1d09be4fa915d5dd591580
Ryan Dawes <dev@dawes.cc>
2025-03-06 12:57:00 -0500

    set bottom menu to open up

--- components/main-menu.tsx
+++ components/main-menu.tsx
@@ -22,18 +22,8 @@ export default function MainMenu() {
 	const protectedClasses = "pointer-events-none bg-gray-100 text-gray-500 dark:bg-gray-100 dark:text-gray-400";
 	return (
 		<Menu>
-			<MenuTrigger asChild>
-				<div className='pt-2 px-0 flex justify-end items-center group'>
-					<button
-						aria-label='Expand Menu'
-						className='size-[1.25rem] relative overflow-clip'>
-						<ImMenu3 className='cursor-pointer size-full absolute inset-0 transition-[scale] timing-spring duration-1000 group-data-[state=closed]:opacity-0 group-data-[state=closed]:scale-95' />
-						<ImMenu4 className='cursor-pointer size-full absolute inset-0 transition-[scale] timing-spring duration-1000 group-data-[state=open]:opacity-0 group-data-[state=open]:scale-95' />
-					</button>
-				</div>
-			</MenuTrigger>
 			<MenuContent>
-				<div className='flex items-center justify-start flex-wrap gap-3 px-1 pb-1'>
+				<div className='flex items-center justify-start flex-wrap gap-3 px-1 pt-1'>
 					<Link
 						href={"https://github.com/dawescc"}
 						className='button'>
@@ -80,6 +70,16 @@ export default function MainMenu() {
 					})}
 				</div>
 			</MenuContent>
+			<MenuTrigger asChild>
+				<div className='pt-2 px-0 flex justify-end items-center group'>
+					<button
+						aria-label='Expand Menu'
+						className='size-[1.25rem] relative overflow-clip'>
+						<ImMenu3 className='cursor-pointer size-full absolute inset-0 transition-[scale] timing-spring group-data-[state=closed]:opacity-0 group-data-[state=closed]:scale-95' />
+						<ImMenu4 className='cursor-pointer size-full absolute inset-0 transition-[scale] timing-spring group-data-[state=open]:opacity-0 group-data-[state=open]:scale-95' />
+					</button>
+				</div>
+			</MenuTrigger>
 		</Menu>
 	);
 }
