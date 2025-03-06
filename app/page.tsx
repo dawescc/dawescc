06f18be9e39523f42d8b08d8f8dc03db9b7b8213
Ryan Dawes <dev@dawes.cc>
2025-03-06 10:06:55 -0500

    updates

--- app/page.tsx
+++ app/page.tsx
@@ -3,16 +3,18 @@ import DawesDesign from "@/components/logos/dsd";
 
 export default function Home() {
 	return (
-		<div className='@container min-h-full max-w-prose mx-auto flex items-center relative'>
-			<div className='font-medium text-sm font-mono uppercase absolute top-0 left-0'>
-				<span className=''>engineering division</span>
-				<span className='flex gap-2 items-center mt-2'>
-					<DawesCC />
-					<DawesDesign className='ml-[3px]' />
-				</span>
-			</div>
-			<div className='text-pretty break-words font-display'>
-				<h1 className='font-black text-7xl '>ryan dawes</h1>
+		<div className='@container min-h-full max-w-prose mx-auto flex relative'>
+			<div className='flex-1 flex flex-col uppercase py-4'>
+				<div className='flex-0 flex gap-2'>
+					<DawesCC className='size-full max-w-1/6' />
+					<DawesDesign className='size-full max-w-1/6' />
+				</div>
+				<div className='flex-1 flex items-center'>
+					<h1 className='font-black text-8xl font-display'>ryan dawes</h1>
+				</div>
+				<div className='flex-0 flex font-mono text-gray-900'>
+					<p className=''>engineering division</p>
+				</div>
 			</div>
 		</div>
 	);
