6d2785fa74954c85c8634ec30db614ad27fda073
Ryan Dawes <dev@dawes.cc>
2025-03-06 11:25:29 -0500

    added threads

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import DawesCC from "@/components/logos/dawescc";
 import DawesDesign from "@/components/logos/dsd";
+import Threads from "@/components/threads";
 
 export default function Home() {
 	return (
-		<div className='@container min-h-full max-w-prose mx-auto flex relative'>
-			<div className='flex-1 flex flex-col uppercase py-4'>
+		<div className='@container min-h-full max-w-prose mx-auto flex'>
+			<div className='flex-1 flex flex-col uppercase py-4 z-[1]'>
 				<div className='flex-0 flex gap-2'>
 					<DawesCC className='size-full max-w-1/6' />
 					<DawesDesign className='size-full max-w-1/6' />
@@ -16,6 +17,12 @@ export default function Home() {
 					<p className=''>engineering division</p>
 				</div>
 			</div>
+			<span className='z-[0] opacity-20 dark:opacity-50'>
+				<Threads
+					amplitude={0.42}
+					distance={1.12}
+				/>
+			</span>
 		</div>
 	);
 }
