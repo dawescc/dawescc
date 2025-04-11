91adda9cf55ed038e1cf7730ff221992dba110ca
Ryan Dawes <dev@dawes.cc>
2025-04-11 17:08:01 -0400

    added clock

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,28 @@
 import ProjectsList from "@/components/projects";
-import { Time, TimeZone } from "@/components/time";
+import { TimeDisplay } from "@/components/time";
 import { siteConfig } from "@/lib/config";
 import { cn } from "@/lib/utils";
 import Link from "next/link";
-import { HiClock } from "react-icons/hi";
 import { SiJavascript, SiTypescript } from "react-icons/si";
 
 export default function Home() {
 	const iconClasses = "inline mb-[5px]";
 	return (
 		<div className='@container min-h-full max-w-2xl flex flex-col px-3 pt-10 pb-5'>
-			<div className='flex-1 flex flex-col gap-12 text-title-1 md:text-title-large tracking-tight'>
+			<div className='flex-1 flex flex-col gap-12 text-title-2 md:text-title-1  tracking-tight'>
 				<div>
-					I&apos;m Ryan, a design engineer on the East Coast{" "}
-					<div className='inline group/hero isolate'>
-						<HiClock className={cn(iconClasses, "z-[3]")} />
-						<span className='text-title-3 inline-flex items-center w-0 overflow-clip whitespace-nowrap max-w-fit group-hover/hero:w-[1000px] -translate-y-1 transition-[width] ease-in-out delay-75 duration-300 z-[1]'>
-							<span className='-translate-x-full group-hover/hero:translate-x-0 transition-[translate] ease-in-out delay-50 duration-300 bg-primary px-1.5 rounded'>
-								<span className='tabular-nums'>
-									<Time />
-								</span>
-								&nbsp;
-								<TimeZone />
-							</span>
-						</span>
-					</div>
+					<span className='text-title-1 md:text-title-large'>I&apos;m Ryan</span>, a design engineer on the East Coast
+				</div>
+
+				<div className='size-32 md:size-36 overflow-clip'>
+					<TimeDisplay />
 				</div>
 
 				<p>
 					I like <SiJavascript className={cn(iconClasses, "text-[#F0DB4F]")} /> && <SiTypescript className={cn(iconClasses, "text-[#3178c6]")} />
 				</p>
-				<p className='text-title-2 md:text-title-1'>
+
+				<p>
 					Find me on{" "}
 					<Link
 						href={siteConfig.links.github}
@@ -38,7 +30,18 @@ export default function Home() {
 						GitHub
 					</Link>
 				</p>
-				<div className='text-title-2 md:text-title-1'>
+
+				<p>
+					While I do have{" "}
+					<Link
+						href={siteConfig.links.twitter}
+						className='rounded [--focus-color:var(--color-primary) underline hover:no-underline focus-visible:no-underline'>
+						Twitter
+					</Link>
+					, I am no longer active on the platform
+				</p>
+
+				<div className='flex gap-x-1 gap-y-1.5 flex-wrap'>
 					<ProjectsList />
 				</div>
 			</div>
